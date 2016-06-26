41ae95f4b42d172824acc99f3d6f104922999ff2
theworldbright <kent@kentshikama.com>
2016-06-26 14:31:42 +0900

    Print message on TextView
    - Handle disconnect click
    - Major refactoring

--- app/src/main/java/com/james/bluetoothreceiver/MainActivity.java
+++ app/src/main/java/com/james/bluetoothreceiver/MainActivity.java
@@ -7,9 +7,9 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,11 +27,10 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
 
     private static final UUID DEFAULT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    TextView largeText;
-    TextView smallText;
-    BluetoothAdapter bluetoothAdapter;
-    BluetoothDevice device;
-    BufferedInputStream inputStream;
+    private TextView connectionMessageView;
+    private TextView messageView;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +38,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        largeText = (TextView) findViewById(R.id.textView_large);
-        smallText = (TextView) findViewById(R.id.textView_small);
+        connectionMessageView = (TextView) findViewById(R.id.connectionMessage);
+        messageView = (TextView) findViewById(R.id.message);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
         if (bluetoothAdapter == null) {
             Toast.makeText(getApplicationContext(), "Device doesnt Support Bluetooth", Toast.LENGTH_SHORT).show();
         }
@@ -75,50 +73,83 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
     public void connect(View view) {
+        BluetoothDevice device = getBluetoothDevice();
+        try {
+            socket = makeSocketConnection(device);
+            final BufferedReader reader = getBufferedReader(socket);
+            pollInput(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Nullable
+    private BluetoothDevice getBluetoothDevice() {
         Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices(); // get connected devices from the adapter
-
-        if (bondedDevices.isEmpty()) { // if the device is connected
+        BluetoothDevice device = null;
+        if (bondedDevices.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Please Pair the Device first", Toast.LENGTH_SHORT).show();
         } else {
             for (BluetoothDevice iterator : bondedDevices) {
-                Log.i("Device", iterator.getAddress());
                 device = iterator;
             }
         }
-        BluetoothSocket socket = null;
-        try {
-            socket = device.createRfcommSocketToServiceRecord(DEFAULT_UUID);
-            socket.connect();
-            inputStream = new BufferedInputStream(socket.getInputStream());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            while (true) {
-                int line = reader.read();
-                Log.i("Device", String.valueOf(line));
+        return device;
+    }
+    private BluetoothSocket makeSocketConnection(BluetoothDevice device) throws IOException {
+        socket = device.createRfcommSocketToServiceRecord(DEFAULT_UUID);
+        socket.connect();
+        setConnectionMessageText("Connected to socket");
+        return socket;
+    }
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private BufferedReader getBufferedReader(BluetoothSocket socket) throws IOException {
+        BufferedInputStream inputStream = new BufferedInputStream(socket.getInputStream());
+        return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+    }
+    private void pollInput(final BufferedReader reader) {
+        new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    try {
+                        String line = reader.readLine();
+                        setMessageText(line);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-            //    MainActivity.this.runOnUiThread(
-            //            new Runnable() {
-            //                public void run() { smallText.append(string); } });
+        }).start();
+    }
+
+    private void setMessageText(final String messageText) {
+        MainActivity.this.runOnUiThread(
+                new Runnable() {
+                    public void run() {
+                        messageView.setText(messageText);
+                    }
+                });
+    }
+    private void setConnectionMessageText(final String messageText) {
+        MainActivity.this.runOnUiThread(
+                new Runnable() {
+                    public void run() {
+                        connectionMessageView.setText(messageText);
+                    }
+                });
+    }
+
+    public void disconnect(View view) {
+        try {
+            socket.close();
+            setConnectionMessageText("Waiting for connection");
+            setMessageText("...");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void disconnect(View view) {
-
-    }
-
-    public void setLargeText(String s) {
-        largeText.setText(s);
-    }
-
-    public void addToSamllText(char c) {
-        smallText.setText(smallText.getText().toString() + c);
-    }
-
     public void resetSmallText(View view) {
-        smallText.setText("");
-
+        messageView.setText("");
     }
 }
